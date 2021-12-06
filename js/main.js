11aa39ec55d76a2d9273f4e6c4c03c2426783ec5
NeverMind109 <vladgladkih123@gmail.com>
2021-12-06 22:38:49 +0300

    Created: Added Adaptive Restonse

--- js/main.js
+++ js/main.js
@@ -1,8 +1,38 @@
-$(function(){
+$(function () {
 // Slick Slider
-    $('.carousel__inner').slick({
-        arrows: false,
-        dots: true,
-        slidesToShow: 3,
-    });
+  $(".carousel__inner").slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 841,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 601,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+// Плавный переход по якорным ссылкам
+  $(".header__content").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr("href"),
+      top = $(id).offset().top;
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
+  $(".services__content-box").on("click", "a", function (event) {
+    event.preventDefault();
+    var id = $(this).attr("href"),
+      top = $(id).offset().top;
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
 });
